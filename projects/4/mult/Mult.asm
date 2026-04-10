9186c84109e38c2b70308f2485c8b08be78212f2
franksants <franklinjuansantos@gmail.com>
2026-04-10 15:30:36 -0300

    Feat Mult.asm implemented using multiplication logic.

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,5 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
+@R2
+M=0
+@INCREMENTATION
+M=0
 
-//// Replace this comment with your code.
+(LOOP)
+  @INCREMENTATION
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JGE
+  @INCREMENTATION
+  M=M+1
+
+  @R0
+  D=M
+  @R2
+  M=M+D
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
